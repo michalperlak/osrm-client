f158569dcbac8bac5b744525266224a8a5013fd0
Michal Perlak <michalperlak@windowslive.com>
2020-07-01 21:57:46 +0200

    Update project build and deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,14 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    java
-    kotlin("jvm") version "1.3.10"
+    kotlin("jvm") version "1.3.72"
 }
 
-group = "pl.michalperlak"
-version = "1.0-SNAPSHOT"
+group = "up.pick"
+version = "1.0.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    compile(kotlin("stdlib-jdk8"))
-    testCompile("junit", "junit", "4.12")
-}
-
-configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-}
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    implementation(kotlin("stdlib"))
 }
